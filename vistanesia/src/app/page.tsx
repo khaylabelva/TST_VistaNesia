dce68291f3e8695878a6593a2b906f0c3c0fa032
Khayla Belva Annandira <belvakhayla@gmail.com>
2024-12-01 17:03:44 +0700

    Update page.tsx

--- vistanesia/src/app/page.tsx
+++ vistanesia/src/app/page.tsx
@@ -141,13 +141,13 @@ export default function Home() {
           <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 w-[90%] bg-gray-500/60 py-2 px-5 rounded-full mx-auto border border-white">
             <div className="flex gap-3 items-center">
               <select className="bg-transparent text-white text-xs border border-gray-300 rounded-md py-0.5 px-1.5 focus:ring-2 focus:ring-gray-500 w-full pointer-events-none">
-                <option>Date</option>
+                <option>Location</option>
               </select>
               <select className="bg-transparent text-white text-xs border border-gray-300 rounded-md py-0.5 px-1.5 focus:ring-2 focus:ring-gray-500 w-full pointer-events-none">
-                <option>Budget</option>
+                <option>Category</option>
               </select>
               <select className="bg-transparent text-white text-xs border border-gray-300 rounded-md py-0.5 px-1.5 focus:ring-2 focus:ring-gray-500 w-full pointer-events-none">
-                <option>Guest</option>
+                <option>Cost</option>
               </select>
               <button className="bg-black text-white text-xs px-4 py-1.5 rounded-md shadow-md pointer-events-none">
                 Search
